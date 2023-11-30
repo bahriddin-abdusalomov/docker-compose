a9f180912deaa380a306b96c73d5932561fe137a
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-30 21:58:36 +0500

    created db context

--- src/Education/Education.Application/Data/AppDbContext.cs
+++ src/Education/Education.Application/Data/AppDbContext.cs
@@ -9,7 +9,9 @@ namespace Education.API.Data;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {}
+    {
+        Database.Migrate();
+    }
 
     public DbSet<User> Users { get; set; }
     public DbSet<Student> Students { get; set; }
