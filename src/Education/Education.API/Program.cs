de80e11146b40d95806ec99669c86916263db9a4
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-30 21:58:53 +0500

    update program cs

--- src/Education/Education.API/Program.cs
+++ src/Education/Education.API/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("Connect")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Docker")));
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
